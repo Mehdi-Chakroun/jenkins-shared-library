43dd9398520a469474914eb6497320f661d8184c
Mehdi-Chakroun <mehdichakroun4@gmail.com>
2023-11-14 23:18:00 +0100

    fix

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -23,7 +23,7 @@ class Docker implements Serializable {
     def deleteUntaggedImage() {
         script.sh "UNTAGGED_IMAGES=\$(aws ecr list-images --repository-name workerservice --filter tagStatus=UNTAGGED --query 'imageIds[*].imageDigest' --output json)"
         script.sh "LATEST_IMAGE=\$(aws ecr list-images --repository-name workerservice --filter tagStatus=TAGGED --query 'imageIds[?imageTag==`latest`].imageDigest' --output json)"
-        script.sh "EXCLUDE_LATEST=\"--image-ids imageDigest=${LATEST_IMAGE}\""
+        script.sh "EXCLUDE_LATEST=\"--image-ids imageDigest=\${LATEST_IMAGE}\""
         script.sh "aws ecr batch-delete-image --repository-name workerservice --image-ids $UNTAGGED_IMAGES $EXCLUDE_LATEST"
     }
 }
